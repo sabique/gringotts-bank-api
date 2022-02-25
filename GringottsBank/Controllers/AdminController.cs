103f4c0f00697a70abe795357a3384749deb351d
Sabique A Khan <sabiquekhan@gmail.com>
2022-02-26 02:20:05 +0530

    Initial Commit

---
+++ GringottsBank/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GringottsBank.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : ControllerBase
+    {
+        public AdminController()
+        {
+
+        }
+    }
+}
